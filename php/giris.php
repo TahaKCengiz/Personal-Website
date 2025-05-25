a829a4ff26aa21659da0189518d821a5042c18a5
TahaKCengiz <kcengiz280@gmail.com>
2025-05-25 06:51:47 +0300

    Hata düzeltmeler.

--- php/giris.php
+++ php/giris.php
@@ -21,7 +21,7 @@
         echo "
             <div class='container-md text-center shadow'>
                 <div class='row text-center'>
-                    <h3 class='Rubik' style='width:400px; margin-left: auto; margin-right: auto;'> Yanlış kullanıcı adı veya şifre! </h3>
+                    <h3 class='Rubik' style='width:400px; margin-left: auto; margin-right: auto;'> Yanlış email adı veya şifre! </h3>
                 </div>
             </div>
             <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js' integrity='sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO' crossorigin='anonymous'></script>
